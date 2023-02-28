5b993d75dd35c07f818ad63a9f158a8acbc959a1
Liza Kuchmenko <elizabeth.kuchmenko@gmail.com>
2023-02-28 10:53:41 +0100

    added animation to boxes

--- src/components/SnackBetter/SnackBetter.jsx
+++ src/components/SnackBetter/SnackBetter.jsx
@@ -1,13 +1,94 @@
-import { motion } from 'framer-motion';
+import { useRef, useEffect, useState } from 'react';
+import { motion, transform } from 'framer-motion';
 
 export function SnackBetter() {
+	const variants = { scale: 1.1, rotate: 6, transition: { duration: 0.3 } };
+
 	return (
-		<div className='h-160 flex justify-center items-center'>
-			<motion.img className='-right-160 top-6 relative scale-75' src='/images/box.png' alt='' />
-			<motion.img className='z-10 top-3 -right-75 relative scale-90' src='/images/box.png' alt='' />
-			<motion.img className='z-20 relative' src='/images/box.png' alt='' />
-			<motion.img className='z-10 top-3 -left-75 relative scale-90 ' src='/images/box.png' alt='' />
-			<motion.img className='-left-160 top-6 relative scale-75' src='/images/box.png' alt='' />
+		<div className='h-160 mx-auto'>
+			<h2 className='text-text text-3xl text-center mt-16 mb-12'>Snack Better.</h2>
+			<motion.div className='flex justify-center items-center'>
+				<motion.img
+					initial={{ opacity: 0, y: 50 }}
+					whileInView={{
+						opacity: 1,
+						y: 0,
+
+						transition: {
+							duration: 0.2,
+							delay: 0.3,
+						},
+					}}
+					whileHover={variants}
+					className='-right-140 relative '
+					src='/images/box-2.png'
+					alt=''
+				/>
+				<motion.img
+					initial={{ opacity: 0, y: 50 }}
+					whileInView={{
+						opacity: 1,
+						y: 0,
+
+						transition: {
+							duration: 0.2,
+							delay: 0.25,
+						},
+					}}
+					whileHover={variants}
+					className='z-10 -right-75 relative'
+					src='/images/box-1.png'
+					alt=''
+				/>
+				<motion.img
+					initial={{ opacity: 0, y: 50 }}
+					whileHover={variants}
+					whileInView={{
+						opacity: 1,
+						y: 0,
+
+						transition: {
+							duration: 0.2,
+							delay: 0.2,
+						},
+					}}
+					className='z-30 relative '
+					src='/images/box.png'
+					alt=''
+				/>
+				<motion.img
+					initial={{ opacity: 0, y: 50 }}
+					whileInView={{
+						opacity: 1,
+						y: 0,
+
+						transition: {
+							duration: 0.2,
+							delay: 0.25,
+						},
+					}}
+					whileHover={variants}
+					className='z-10 -left-75 relative '
+					src='/images/box-1.png'
+					alt=''
+				/>
+				<motion.img
+					initial={{ opacity: 0, y: 50 }}
+					whileInView={{
+						opacity: 1,
+						y: 0,
+
+						transition: {
+							duration: 0.2,
+							delay: 0.3,
+						},
+					}}
+					whileHover={variants}
+					className='-left-140 relative'
+					src='/images/box-2.png'
+					alt=''
+				/>
+			</motion.div>
 		</div>
 	);
 }
