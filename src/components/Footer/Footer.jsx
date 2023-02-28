6271a2877f4029cb3f63a822c0922e191bf05bc3
Liza Kuchmenko <elizabeth.kuchmenko@gmail.com>
2023-02-28 12:16:17 +0100

    fixed footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 export function Footer() {
+	const sections = ['Mango', 'Banana', 'Pineapple', 'Pitahaya', 'Variety', 'Sampler'];
 	return (
 		<div className='h-auto text-text max-w-[1050px] mx-auto pb-20'>
 			<div className='w-full h-28 flex justify-between items-end'>
@@ -14,6 +15,16 @@ export function Footer() {
 						className='bg-primary border-b border-grey w-96 pb-2 placeholder:text-text '
 					/>
 					<span className='absolute right-0'>→</span>
+				</div>
+			</div>
+			<div className='flex justify-start mt-20'>
+				<div className='flex flex-col'>
+					{sections.map((section) => (
+						<div className='flex justify-between w-110 border-b border-grey mt-8'>
+							<p className='text-sm'>{section}</p>
+							<div>→</div>
+						</div>
+					))}
 				</div>
 			</div>
 		</div>
