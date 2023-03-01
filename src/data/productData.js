d9eb712fb8da3cb6c60143b0b1dd38cdb49ae90f
Liza Kuchmenko <elizabeth.kuchmenko@gmail.com>
2023-03-01 18:01:01 +0100

    removed data

--- src/data/productData.js
+++ src/data/productData.js
@@ -35,27 +35,4 @@ export const productData = [
 			'Pineapples straight from the warm shores of Costa Rica. Crunchy. Vegan. Just guilt free snacking. 12 packs.',
 		images: ['banana0.jpg', 'mango0.jpg', 'pineapple2.jpeg', 'mango0.jpg', 'banana0.jpg'],
 	},
-	{
-		id: 4,
-		name: 'pitahaya',
-		price: [
-			{ quantity: 3, price: 9 },
-			{ quantity: 5, price: 15 },
-			{ quantity: 10, price: 29 },
-		],
-		description:
-			'Our most exotic flavor. Like you are eating a dragon fruit at the beach in Thailand. Crunchy. Vegan. Just guilt free snacking. 12 packs.',
-		images: ['banana0.jpg', 'mango0.jpg', 'banana2.jpeg', 'mango0.jpg', 'banana0.jpg'],
-	},
-	{
-		id: 5,
-		name: 'variety',
-		price: [
-			{ quantity: 3, price: 9 },
-			{ quantity: 5, price: 15 },
-			{ quantity: 10, price: 29 },
-		],
-		description: 'A mix of all our flavors. Crunchy. Vegan. Just guilt free snacking. 12 packs.',
-		images: ['banana0.jpg', 'mango0.jpg', 'pineapple2.jpeg', 'mango0.jpg', 'banana0.jpg'],
-	},
 ];
