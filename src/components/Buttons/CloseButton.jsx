46985b3ade2c1264ab19a214a59cfdeb43623d9d
Liza Kuchmenko <elizabeth.kuchmenko@gmail.com>
2023-03-17 12:57:38 +0100

    fixed styles

--- src/components/Buttons/CloseButton.jsx
+++ src/components/Buttons/CloseButton.jsx
@@ -1,19 +1,46 @@
-import { motion } from 'framer-motion';
+import { motion, useInView } from 'framer-motion';
 import { useAppContext } from '../Context';
+import { useRef } from 'react';
 
 export function CloseButton() {
 	const { setOpenMenu } = useAppContext();
+	const ref = useRef(null);
+	const isInView = useInView(ref);
+	const buttonRef = useRef(null);
+
+	function onTap(event, info) {
+		console.log(info.point.x, info.point.y);
+		buttonRef.current.style.transform = 'rotate(45deg)';
+	}
+
 	return (
-		<motion.button className='h-2.5 w-5 flex relative cursor-pointer pl-2 mt-0.5' onClick={() => setOpenMenu(false)}>
+		<motion.button
+			ref={buttonRef}
+			onTap={onTap}
+			className='h-2.5 w-5 flex relative cursor-pointer pl-2 mt-0.5'
+			onClick={() => setOpenMenu(false)}
+		>
 			<motion.div
-				animate={{ rotate: '225deg' }}
-				transition={{ ease: 'linear', duration: 0.5 }}
+				initial='hidden'
+				whileInView='visible'
+				viewport={{ once: true }}
+				transition={{ ease: 'linear', duration: 0.3 }}
+				variants={{
+					visible: { rotate: '135deg' },
+					hidden: { rotate: 0 },
+				}}
 				className='w-5 h-0.5 bg-primary absolute'
 			></motion.div>
 			<motion.div
-				animate={{ rotate: '135deg', width: '20px' }}
-				transition={{ ease: 'linear', duration: 0.5 }}
-				className='w-3 h-0.5 bg-primary absolute'
+				initial='hidden'
+				whileInView='visible'
+				viewport={{ once: true }}
+				transition={{ ease: 'linear', duration: 0.3 }}
+				variants={{
+					visible: { rotate: '45deg', width: '20px' },
+					hidden: { rotate: 0, width: '12px' },
+				}}
+				className=' h-0.5 bg-primary absolute'
 			></motion.div>
 		</motion.button>
 	);
